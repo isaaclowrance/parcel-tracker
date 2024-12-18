97af1828c9b3f816693de166a29f4ea2519c091e
isaaclowrance <isaaclowrance@icloud.com>
2024-12-18 10:00:00 +0530

    first commit

---
+++ src/parcel/tracker/ParcelTracker.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package parcel.tracker;
+
+/**
+ *
+ * @author rashm
+ */
+public class ParcelTracker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
